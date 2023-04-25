b2896c4ccbf0ab8a1c0a3aaa03e2f1e5b2923528
MilchRatchet <milchratchet@googlemail.com>
2023-04-25 20:38:55 +0200

    File names are now converted to lowercase for hashing.

--- ceb_base.c
+++ ceb_base.c
@@ -1,12 +1,13 @@
 #include "ceb_base.h"
 
+#include <ctype.h>
 #include <stdint.h>
 
 uint64_t ceb_hash_fnv1a(const char* str) {
   uint64_t hash = 0xcbf29ce484222325;
 
   for (size_t i = 0; str[i] != '\0'; i++) {
-    hash = hash ^ ((uint64_t) str[i]);
+    hash = hash ^ ((uint64_t) tolower(str[i]));
     hash = hash * 0x100000001b3;
   }
 
